e16308ea76a6cc40517a7addef06a006940a613e
Softwarica College Lab <softwarica@softwarica.com>
2019-04-07 11:44:55 +0545

    backend

--- app/src/main/java/com/example/bmirupesh/MainActivity.java
+++ app/src/main/java/com/example/bmirupesh/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.bmirupesh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.EventListener;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn1;
     private EditText etheight,etweight;
 
@@ -13,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etheight=findViewById(R.id.etheight);
+        etweight=findViewById(R.id.etweight);
+        btn1=findViewById(R.id.btnclick);
+        btn1.setOnClickListener(this);
+
+        double height= Double.parseDouble(etheight.getText().toString());
+        double weight= Double.parseDouble(etweight.getText().toString());
+
+    }
+    @Override
+    public void onClick(View v){
+
+
+
     }
 }
