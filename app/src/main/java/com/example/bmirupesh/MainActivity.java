ea45677d3cc87f3802d874d7ee83534c021cee16
Softwarica College Lab <softwarica@softwarica.com>
2019-04-07 11:24:03 +0545

    Initial commit

---
+++ app/src/main/java/com/example/bmirupesh/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.bmirupesh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+    private EditText etheight,etweight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
