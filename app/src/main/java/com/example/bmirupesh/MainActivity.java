445054ba29e8e9ef4f6e5ffdca74d9fc7c1c08c9
rupesh pandey <rupesh160136>
2019-04-08 16:46:08 +0545

    toast added

--- app/src/main/java/com/example/bmirupesh/MainActivity.java
+++ app/src/main/java/com/example/bmirupesh/MainActivity.java
@@ -5,30 +5,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.EventListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn1;
-    private EditText etheight,etweight;
+    private EditText etheight, etweight;
+    private TextView tvresult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etheight=findViewById(R.id.etheight);
-        etweight=findViewById(R.id.etweight);
-        btn1=findViewById(R.id.btnclick);
+        etheight = findViewById(R.id.etheight);
+        etweight = findViewById(R.id.etweight);
+        btn1 = findViewById(R.id.btnclick);
+        tvresult = findViewById(R.id.tvout);
         btn1.setOnClickListener(this);
-
-        double height= Double.parseDouble(etheight.getText().toString());
-        double weight= Double.parseDouble(etweight.getText().toString());
-
     }
+
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
+        float height = Float.parseFloat(etheight.getText().toString()) / 100;
+        float weight = Float.parseFloat(etweight.getText().toString());
+        float result = weight / (height * height);
+        tvresult.setText(Float.toString(result));
 
-
-
+        if (result <= 18.5) {
+            Toast.makeText(getApplicationContext(), "you are under weight", Toast.LENGTH_LONG).show();
+        } else if (result > 18.5 & result < 24.9) {
+            Toast.makeText(getApplicationContext(), "normal", Toast.LENGTH_LONG).show();
+        }else if (result>25 & result<29.9) {
+            Toast.makeText(getApplicationContext(),"over weight",Toast.LENGTH_LONG).show();
+        }
     }
+
 }
+
+
